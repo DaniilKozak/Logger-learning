7d8e78c1f6318d89d7a62f0b0d1cac266f813251
Daniil <DDCBrazl@yandex.ru>
2025-12-06 16:13:44 +0500

    Added class to test logger

--- src/main/java/com/babaev/learning/LoggerTest.java
+++ src/main/java/com/babaev/learning/LoggerTest.java
@@ -6,7 +6,11 @@ import org.slf4j.LoggerFactory;
 public class LoggerTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerTest.class);
     public static void main(String... a) {
-        LOGGER.info("Hello World");
+        LOGGER.trace("Console logger test trace");
+        LOGGER.debug("Console logger test debug");
+        LOGGER.info("Console logger test info");
+        LOGGER.warn("Console logger test error");
+        LOGGER.error("Console logger test error");
 
     }
 }
